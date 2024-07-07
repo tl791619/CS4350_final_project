02c4d1b5cb1cb186b422b681ddb4b2695a16cf3f
tl791619 <tl791619@ohio.edu>
2024-07-07 08:12:42 -0400

    add event callback class files

---
+++ src/MazeEventCallback.cpp
@@ -0,0 +1,6 @@
+/*
+* @file "MazeEventCallback.cpp"
+* @author Tatiana Lipert
+* @date 2024-07-07
+* @brief inheritted PxSimulationEventCallback for onContact() event callback
+*/
---
+++ src/MazeEventCallback.h
@@ -0,0 +1,6 @@
+/* 
+* @file "MazeEventCallback.h" 
+* @author Tatiana Lipert 
+* @date 2024-07-07
+* @brief inheritted PxSimulationEventCallback for onContact() event callback 
+*/
